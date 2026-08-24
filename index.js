d770fa452f6883f613b9fd2fdecd9de5c53ac8cd
FerdosFijer <ffijer@gmail.com>
2026-08-24 12:46:01 +0600

    Done vercel and deployment work 2

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const db = client.db("hireloop_db");
     const jobcollection = db.collection("jobs");
     const companyCollection = db.collection("companies");
